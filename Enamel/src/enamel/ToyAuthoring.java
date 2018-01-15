bb2a04ea166ab87afe5b4d1902dc36b94e1afe18
danielkozlovsky <daniel.kozlovsky@gmail.com>
2018-01-15 12:13:58 -0500

    Proof of commit

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -2,8 +2,8 @@ package enamel;
 
 public class ToyAuthoring {
 
-	// will be replaced by Authoring App
-    public static void main(String[] args) { 	  	
+	// will be replaced by Authoring App.
+    public static void main(String[] args) { 	   	
     	    ScenarioParser s = new ScenarioParser(true);
     	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
     }
