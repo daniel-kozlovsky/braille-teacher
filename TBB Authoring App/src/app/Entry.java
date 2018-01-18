48acacd5ec4c6677ea45c3620811d32ccf2df38f
danielkozlovsky <daniel.kozlovsky@gmail.com>
2018-01-18 12:34:46 -0500

    Improved entry point of the program to avoid GUI hang

--- TBB Authoring App/src/app/Entry.java
+++ TBB Authoring App/src/app/Entry.java
@@ -1,10 +1,21 @@
 package app;
 
+import javax.swing.SwingUtilities;
+
 public class Entry {
 
 	public static void main(String[] args) {
 	
-		MainScreen m = new MainScreen();
+		SwingUtilities.invokeLater( new Runnable() {
+
+			@Override
+			public void run() {
+				final MainScreen m = new MainScreen();
+				m.setVisible(true);
+			}
+			
+		});
+		
 		
 
 	}
