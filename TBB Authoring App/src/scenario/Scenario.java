fbd64695cb0609da0faed64b0607bb0643c294cb
daniel kozlovsky <daniel.kozlovsky@gmail.com>
2018-02-02 01:24:53 -0500

    Finished implementation of Scenario

--- TBB Authoring App/src/scenario/Scenario.java
+++ TBB Authoring App/src/scenario/Scenario.java
@@ -18,50 +18,88 @@ public class Scenario {
 	
 	private ArrayList<ScenarioCommand> runningScenario;
 	
+	/**
+	 * Default constructor; sets to 0
+	 */
 	public Scenario()
 	{
-		//TODO: Implement
-		
+		this(0,0);	
+	}
+	
+	/**
+	 * Set number of braille cells and buttons
+	 * @param numcells
+	 * @param numbuttons
+	 */
+	public Scenario(int numcells, int numbuttons)
+	{
+		numCells = numcells;
+		numButtons = numbuttons;
+		runningScenario = new ArrayList<ScenarioCommand>();
+	}
+	
+	/**
+	 * get the number of cells. Required for formatting.
+	 * @return number of cells
+	 */
+	public int getNumCells()
+	{
+		return numCells;
+	}
+	
+	/**
+	 * get the number of buttons. Required for formatting
+	 * @return number of buttons
+	 */
+	public int getNumButtons()
+	{
+		return numButtons;
 	}
 	
 	public ScenarioCommand getEvent(int index)
 	{
-		//TODO: Implement
 		
-		return null;
+		return runningScenario.get(index);
 	}
 	
 	/**
 	 * Adds an event to the end of the scenario.
 	 * 
-	 * @param event The event or action to be added
+	 * @param command The event or action to be added
 	 */
-	public void addEvent(ScenarioCommand event)
+	public void addEvent(ScenarioCommand command)
 	{
-		//TODO: Implement
+		runningScenario.add(command);
 	}
 	
 	/**
 	 * Removes an event from the scenario.
 	 * 
-	 * @param event The event to be removed
+	 * @param command The event to be removed
 	 */
-	public void removeEvent(ScenarioCommand event)
+	public void removeEvent(ScenarioCommand command)
 	{
-		//TODO: Implement
+		runningScenario.remove(command);
 	}
 	
 	/**
-	 * Moves an event from its current position to a new position. 
-	 * Removes the event at the current index and inserts into specified 
-	 * index, shifting all events after, further down. 
+	 * Moves an event from its current position to a new position. Elements are shifted left 
+	 * after removal, then right after insertion.
 	 * 
-	 * @param event The event to move
-	 * @param newIndex The new position of the event
+	 * @param command The event to move
+	 * @param newIndex The new position of the event. Cannot be outside the list
+	 * @throws IllegalArgumentException if newIndex is not in the list
 	 */
-	public void moveEvent(ScenarioCommand event, int newIndex)
+	public void moveEvent(ScenarioCommand command, int newIndex) throws IllegalArgumentException
 	{
-		//TODO: Implement
+		if(newIndex > runningScenario.size()-1 || newIndex < 0)
+		{
+			throw new IllegalArgumentException("newIndex must be within the list!");
+		}
+		runningScenario.remove(command);
+		runningScenario.add(newIndex, command);
+		
+		
 	}
 
 }
