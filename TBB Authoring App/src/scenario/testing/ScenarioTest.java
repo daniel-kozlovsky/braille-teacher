b3df950df3d15e941c64c3e028bc3c3a8ca2ff80
daniel kozlovsky <daniel.kozlovsky@gmail.com>
2018-02-04 04:44:34 -0500

    Added test package and test frame for Scenario

---
+++ TBB Authoring App/src/scenario/testing/ScenarioTest.java
@@ -0,0 +1,14 @@
+package scenario.testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ScenarioTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
