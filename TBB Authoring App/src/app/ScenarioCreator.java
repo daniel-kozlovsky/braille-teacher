5a7044e7287939a0c768967bf138aad5c5b370e2
ArshdeepSaini <softdev.arshdeep@gmail.com>
2018-02-10 13:57:42 -0500

    manual merge - branch: scenario_creator

--- TBB Authoring App/src/app/ScenarioCreator.java
+++ TBB Authoring App/src/app/ScenarioCreator.java
@@ -1,6 +1,8 @@
 package app;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.ImageObserver;
 
 import javax.swing.*;
@@ -10,18 +12,69 @@ import scenario.Scenario;
 public class ScenarioCreator extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+    private JPanel panel = new JPanel();
+    private JButton btnAddCommand = new JButton("Add command");
+    private JButton btnRemove = new JButton("Remove");
+    private JButton btnMoveUp = new JButton("Move Up");
+    private JButton btnMoveDown = new JButton("Move Down");
+    private JButton btnEdit = new JButton("Edit");
+    private JButton btnRepeat = new JButton("Repeat");
+    private String cell;
+    private String buttons;
 	private JFrame parent;
 	private Scenario importedScenario;
 	private JButton addAudioButton;
 
 	public ScenarioCreator(JFrame parent, Scenario importedScenario) {
 		this.parent = parent;
+        this.setLayout(null);
 		this.importedScenario = importedScenario;
-		initComponents();
-		this.parent.setSize(parent.getWidth(), 300);
+        this.parent.setTitle("Creat new Scenario");
+        // creating new  window to add the number of cells
+        JOptionPane jcell= new JOptionPane();
+        jcell.getAccessibleContext().setAccessibleDescription(" How many cells do you need ");
+        cell=   jcell.showInputDialog(parent," How many cells do you need ",null);
+        //creating new window to add the numnber of buttons
+        JOptionPane jbutton= new JOptionPane();
+        jbutton.getAccessibleContext().setAccessibleDescription(" How many buttons do you need ");
+        buttons =   jbutton.showInputDialog(parent," How many buttons do you need ",null);
+    initComponents();
+    this.parent.setSize(parent.getWidth(), 400);
+		
+		
+		
 	}
 
 	private void initComponents() {
+        
+        panel.setBounds(6, 10, 272, 513);
+        add(panel);
+        
+        btnAddCommand.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //addCommand command = new addCommand();
+            }
+        });
+
+        btnAddCommand.setBounds(288, 20, 117, 29);
+        add(btnAddCommand);
+        
+        btnRemove.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+        btnRemove.setBounds(288, 69, 117, 29);
+        add(btnRemove);
+        btnMoveUp.setBounds(288, 119, 117, 29);
+        add(btnMoveUp);
+        btnMoveDown.setBounds(288, 173, 117, 29);
+        add(btnMoveDown);
+        btnEdit.setBounds(288, 233, 117, 29);
+        add(btnEdit);
+        btnRepeat.setBounds(288, 288, 117, 29);
+        add(btnRepeat);
+        
 		JTextArea audioTextField = new JTextArea();
 		audioTextField.getAccessibleContext().setAccessibleDescription("Textfield for speaker audio script.");
 		addAudioButton = new JButton("Add Audio");
@@ -39,5 +92,6 @@ public class ScenarioCreator extends JPanel {
 		this.add(audioTextField);
 
 	}
-
 }
+
+
