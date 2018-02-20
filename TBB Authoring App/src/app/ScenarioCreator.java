07b265dc79b3038a9a1168a0bbfa47411cfc60c5
daniel kozlovsky <daniel.kozlovsky@gmail.com>
2018-02-20 06:11:22 -0500

    removed unused imports

--- TBB Authoring App/src/app/ScenarioCreator.java
+++ TBB Authoring App/src/app/ScenarioCreator.java
@@ -1,14 +1,8 @@
 package app;
 import java.awt.*;
-import java.awt.TrayIcon.MessageType;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Random;
-
 import javax.swing.*;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
